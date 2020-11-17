2b6f195b400125c0894738a59017a54210fdb882
CarolGutierrez <neylin_gutierrez_@hotmail.com>
2020-11-17 23:09:24 +0100

    privado

--- src/proyectofuturo/ProyectoFuturo.java
+++ src/proyectofuturo/ProyectoFuturo.java
@@ -18,7 +18,7 @@ public class ProyectoFuturo {
         System.out.println("Estudiante: "+args[0]);
         System.out.println(veredicto(args[1], args[2]));
     }
-    static String veredicto(String valor1, String valor2){
+    private static String veredicto(String valor1, String valor2){
          if(valor1.equals(valor2)){
              return "El dedicar 1 hora puede ser insuficiente";
          }

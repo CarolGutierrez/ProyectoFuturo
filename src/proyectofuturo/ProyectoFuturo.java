593ed1d1b73f7dc34b311df018a400c5272c01e2
CarolGutierrez <neylin_gutierrez_@hotmail.com>
2020-11-17 23:07:14 +0100

    Inicio

---
+++ src/proyectofuturo/ProyectoFuturo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofuturo;
+
+/**
+ *
+ * @author Neylin
+ */
+public class ProyectoFuturo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Estudiante: "+args[0]);
+        System.out.println(veredicto(args[1], args[2]));
+    }
+    static String veredicto(String valor1, String valor2){
+         if(valor1.equals(valor2)){
+             return "El dedicar 1 hora puede ser insuficiente";
+         }
+         if(Integer.valueOf(valor1)> Integer.valueOf(valor2) ){
+             return "deberás dedicar mas horas si quieres aprobar";
+         }
+    return "Ideal trabaja los contenidos en casa";
+    }
+    
+}
